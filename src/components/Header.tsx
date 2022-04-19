0bc842282ad4c9f335e8e6153ab9e93f39ffb7e1
David Zadok, Jr <david.zadok.jr@gmail.com>
2022-04-18 21:55:02 -0400

    Setup Firebase

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <h1>
-        David Zadok
-        <span className="links">
-          <a href="https://github.com/dzadok">github</a>&nbsp;
-          <a href="mailto:david.zadok.jr@gmail.com">email</a>&nbsp;
-          <a href="tel:1231231234">phone</a>
-        </span>
-      </h1>
+      David Zadok
+      <span className="links">
+        <a href="https://github.com/dzadok">github</a>&nbsp;
+        <a href="mailto:david@dzadok.dev">email</a>&nbsp;
+      </span>
     </header>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Blog from "./components/Blog";
 import Header from "./components/Header";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBt93YZMu-obz_vgwlSPqzlOlCV8F0BCno",
+  authDomain: "dzadok-dev.firebaseapp.com",
+  projectId: "dzadok-dev",
+  storageBucket: "dzadok-dev.appspot.com",
+  messagingSenderId: "161116533438",
+  appId: "1:161116533438:web:e080636504cd8b2cf3a4ef",
+  measurementId: "G-PDK1GZ5R4C",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
