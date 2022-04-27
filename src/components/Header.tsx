7322b019bf2056a301d2fc842254084435f5d804
David Zadok, Jr <david.zadok.jr@gmail.com>
2022-04-26 22:23:39 -0400

    Replace text links with icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
-import React from "react";
+import EmailIcon from "./Email";
+import GithubIcon from "./Github";
 import "./Header.css";
+import LinkedInIcon from "./Linkedin";
 
 export default function Header() {
   return (
     <header>
       <span className="myName">David Zadok</span>
       <span className="links">
-        <a href="https://github.com/dzadok">github</a>&nbsp;
-        <a href="mailto:david@dzadok.dev">email</a>&nbsp;
+        <a href="https://github.com/dzadok">{GithubIcon()}</a>
+        <a href="mailto:david@dzadok.dev">{EmailIcon({})}</a>
+        <a href="https://linkedin.com/in/david-zadok">{LinkedInIcon()}</a>
       </span>
     </header>
   );
