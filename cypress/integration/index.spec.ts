082f76160d0d6f7a7cf9379a3af1c49c3a447a2f
David Zadok, Jr <david.zadok.jr@gmail.com>
2022-05-12 12:39:58 -0400

    Fix tests

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -5,11 +5,11 @@ describe("Visit Main page", () => {
     cy.visit("").get("title").should("contain.text", "David Zadok");
   });
   it("Has my name on the page", () => {
-    cy.visit("").get(".myName").should("contain.text", "David Zadok");
+    cy.visit("").get("header > .myName").should("contain.text", "David Zadok");
   });
   it('Display markdown with a title starting with "Motivation"', () => {
     cy.visit("")
-      .get("#blog > :first-child")
+      .get("#blog > :first-child > h1")
       .should("include.text", "Motivation");
   });
   it("Should show the blog post date", () => {
