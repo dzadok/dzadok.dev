41811b3b494e7ad8cd3a65374e691e4439729998
David Zadok, Jr <david.zadok.jr@gmail.com>
2022-05-19 08:11:14 -0400

    Update targeted browsers

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["./content/*.md"],
+  build: {
+    target: ["chrome89", "edge89", "firefox89", "ios15", "opera86", "safari15"],
+  },
 });
