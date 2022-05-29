c37d0405169e851a13876c9c4faa48cd1430f784
David Zadok, Jr <david.zadok.jr@gmail.com>
2022-05-29 13:12:07 -0400

    Only load one blog post at a time

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -27,64 +27,69 @@ const db = getFirestore(app);
 const collRef = collection(db, "blogPosts");
 const posts = (await getDoc(doc(db, "BlogPostX/byDate"))).get("ids");
 
-async function GetBlogPosts() {
-  const content = [];
-  for (const id of posts) {
-    content.push(await getDoc(doc(collRef, id)));
-  }
-  return content;
-}
-
-function getLastPostFromLocalStorage(dft = 0) {
-  const id = localStorage.getItem("blogPostId");
-  return id ? parseInt(id) : dft;
-}
-
 export default function Blog() {
   const { theme } = useContext(ThemeContext);
   const [blogPosts, updateBlogPosts] = useState(
-    new Array<DocumentSnapshot<DocumentData>>()
+    new Map<number, DocumentSnapshot<DocumentData>>()
   );
-  const [postId, updatePostId] = useState(getLastPostFromLocalStorage(0));
+  const [postId, updatePostId] = useState(-1);
 
-  function firstPost() {
-    localStorage.setItem("blogPostId", "0");
+  function getLastPostFromLocalStorage(dft = 0) {
+    const storage = localStorage.getItem("blogPostId");
+    return storage ? parseInt(storage) : dft;
+  }
+
+  async function getPost(postId: number) {
+    if (postId === -1) {
+      return false;
+    }
+    if (!blogPosts.has(postId)) {
+      await getDoc(doc(collRef, posts[postId])).then((post) => {
+        updateBlogPosts(blogPosts.set(postId, post));
+      });
+    }
+    localStorage.setItem("blogPostId", postId.toString());
+    return true;
+  }
+
+  async function firstPost() {
+    await getPost(0);
     updatePostId(0);
   }
 
-  function prevPost() {
+  async function prevPost() {
     const newId = postId - 1;
-    localStorage.setItem("blogPostId", newId.toString());
+    await getPost(newId);
     updatePostId(newId);
   }
 
-  function nextPost() {
+  async function nextPost() {
     const newId = postId + 1;
-    localStorage.setItem("blogPostId", newId.toString());
+    await getPost(newId);
     updatePostId(newId);
   }
 
-  function lastPost() {
-    const newId = blogPosts.length - 1;
-    localStorage.setItem("blogPostId", newId.toString());
+  async function lastPost() {
+    const newId = posts.length - 1;
+    await getPost(newId);
     updatePostId(newId);
   }
 
   useEffect(() => {
-    GetBlogPosts().then((posts) => {
-      updateBlogPosts(posts);
+    const postNumber = getLastPostFromLocalStorage(0);
+    getPost(postNumber).then((updated) => {
+      if (updated) {
+        updatePostId(postNumber);
+      }
     });
   }, []);
 
-  const blogPost =
-    blogPosts.length > 0 ? (
-      <BlogPost
-        lightOrDarkTheme={theme}
-        blogPost={blogPosts[postId]}
-      ></BlogPost>
-    ) : (
-      "Loading"
-    );
+  const currentPost = blogPosts.get(postId);
+  const blogPost = currentPost ? (
+    <BlogPost lightOrDarkTheme={theme} blogPost={currentPost}></BlogPost>
+  ) : (
+    "Loading"
+  );
 
   return (
     <section id="blog">
@@ -96,10 +101,10 @@ export default function Blog() {
         <button onClick={prevPost} disabled={postId === 0}>
           Previous
         </button>
-        <button onClick={nextPost} disabled={postId + 1 >= blogPosts.length}>
+        <button onClick={nextPost} disabled={postId + 1 >= posts.length}>
           Next
         </button>
-        <button onClick={lastPost} disabled={postId + 1 >= blogPosts.length}>
+        <button onClick={lastPost} disabled={postId + 1 >= posts.length}>
           Last
         </button>
       </nav>
