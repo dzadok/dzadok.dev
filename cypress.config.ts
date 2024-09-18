50ca1f9c9eeaab132a2cf26d0eb0fdb6027b3edd
Dave Zadok <david@dzadok.dev>
2024-09-17 21:22:39 -0400

    Update cypress port

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      return require("./cypress/plugins/index.ts")(on, config);
     },
-    baseUrl: 'http://localhost:3000/',
-    specPattern: 'cypress/e2e/**/*{test,spec}.{ts,tsx}',
+    baseUrl: "http://localhost:5173/",
+    specPattern: "cypress/e2e/**/*{test,spec}.{ts,tsx}",
   },
   component: {
     setupNodeEvents(on, config) {},
-    specPattern: './src//**/*{test,spec}.{ts,tsx}',
+    specPattern: "./src//**/*{test,spec}.{ts,tsx}",
   },
-})
+});
